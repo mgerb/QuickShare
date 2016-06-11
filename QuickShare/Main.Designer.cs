6da0efb38b3e8fcdd6cd987edb18556ada603306
mgerb42 <mgerb42@gmail.com>
2016-06-11 15:57:04 -0500

    init

---
+++ QuickShare/Main.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace QuickShare
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Text Box";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hotkey: Shift+F4";
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Config";
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QuickShare/Main.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickShare
+{
+    public partial class Main : Form
+    {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+
+        public Main()
+        {
+            InitializeComponent();
+            notifyIcon1.Visible = true;
+            textBox1.Text = Properties.Settings.Default["folderPath"].ToString();
+
+            int id = 2;     // The id of the hotkey. 
+            RegisterHotKey(this.Handle, id, (int)KeyModifier.Shift, Keys.F4.GetHashCode());
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+
+                ShowBalloonAndUpdate(notifyIcon1, 500, "Minimized", "Press Shift+F4 to take a screenshot.");
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {   
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                textBox1.Text = fbd.SelectedPath;
+                Properties.Settings.Default["folderPath"] = fbd.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                try
+                {
+                    Bitmap bitmap = ScreenCapturer.Capture(enmScreenCaptureMode.Window);
+                    Properties.Settings.Default["folderPath"] = textBox1.Text;
+                    Properties.Settings.Default.Save();
+
+                    String folderPath = textBox1.Text + "\\";
+                    String imageName = bitmap.GetHashCode() + RandomString(4) + ".jpeg";
+                    
+
+                    while (File.Exists(folderPath + imageName))
+                    {
+                        imageName = bitmap.GetHashCode() + RandomString(4) + ".jpeg";
+                    }
+
+                    bitmap.Save(folderPath + imageName, ImageFormat.Jpeg);
+                    ShowBalloonAndUpdate(notifyIcon1, 500, "Screenshot", folderPath);
+                }
+                catch (Exception)
+                {
+                    ShowBalloonAndUpdate(notifyIcon1, 500, "Error", "Invalid folder path...");
+                }
+
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            NotifyIcon ni = sender as NotifyIcon;
+
+            try
+            {
+                Process.Start(@ni.BalloonTipText);
+            }
+            catch (Exception)
+            {
+                
+            }
+
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public void ShowBalloonAndUpdate(NotifyIcon ni, int timeout, string title, string text)
+        {
+            ni.BalloonTipTitle = title;
+            ni.BalloonTipText = text;
+            ni.ShowBalloonTip(timeout);
+        }
+
+        
+    }
+}
